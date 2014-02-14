f83f416dcc3646ed7bba2486a660c324e0d03326
Wei Gao <jky239@gmail.com>
2014-02-14 14:12:34 +0800

    init

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,27 +2,15 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
         jshint: {
-            all: ['Gruntfilejs', 'lib/**/*.js'],
+            all: ['Gruntfilejs', 'bin/*.js', 'lib/**/*.js'],
             options: {
                 jshintrc: '.jshintrc'
             }
         },
-        watch: {
-            options: {
-                nospawn: false
-            },
-            js: {
-                files:['lib/**/*.js'],
-                tasks: ['jshint']
-            }
-        }
+        
     });
 
-
     grunt.loadNpmTasks("grunt-contrib-jshint");
-    grunt.loadNpmTasks("grunt-contrib-copy");
-    grunt.loadNpmTasks("grunt-contrib-clean");
-    grunt.loadNpmTasks("grunt-contrib-watch");
     
     grunt.registerTask("default", ["jshint"]);
 };
